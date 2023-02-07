8265a42b9954dd4fc00dd239cd268bf7548247a1
Emily Sellers <emilymelindasellers@gmail.com>
2023-02-06 16:41:55 -0800

    add component file structure, auth and bulletins routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header/Header.js';
+import Auth from './components/Auth.js';
+import Bulletins from './components/Bulletins/Bulletins.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/bulletins" component={Bulletins} />
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/Bulletins/Bulletins.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Bulletins() {
+  return <div>List of Bulletins</div>;
+}
