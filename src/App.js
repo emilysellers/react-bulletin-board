fc5a9f4446e2d6f0ec2382e3cd5d1b1590db83e9
Emily Sellers <emilymelindasellers@gmail.com>
2023-02-06 21:44:06 -0800

    enable user sign up or sign in and redirect to bulletins for authenticated user

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import { Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header.js';
 import Auth from './components/Auth.js';
 import Bulletins from './components/Bulletins/Bulletins.js';
+import { useUser } from './context/UserContext.js';
 
 function App() {
+  const { user } = useUser();
   return (
-    <>
+    <div>
       <Header />
       <Switch>
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth/:type" component={Auth} />
         <Route path="/bulletins" component={Bulletins} />
+        <Route exact path="/">
+          <>
+            {user && <Redirect to="/bulletins" />}
+            {!user && <Redirect to="/auth/sign-in" />}
+          </>
+        </Route>
       </Switch>
-    </>
+    </div>
   );
 }
 
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,5 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink, Redirect, useParams } from 'react-router-dom';
+import { useUser } from '../context/UserContext.js';
+import { authUser } from '../services/auth.js';
 
 export default function Auth() {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { type } = useParams();
+  const { user, setUser } = useUser();
+  if (user) {
+    return <Redirect to="/bulletins" />;
+  }
+
+  const submitAuth = async () => {
+    try {
+      const newUser = await authUser(email, password, type);
+      console.log(newUser);
+      setUser(newUser);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <div>
+      <p>Welcome! Please sign in or sign up</p>
+      <div>
+        <NavLink to="/auth/sign-in">Sign in</NavLink>
+        <NavLink to="/auth/sign-up">Sign up</NavLink>
+      </div>
+      <label>Email</label>
+      <input
+        type="email"
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <label>Password</label>
+      <input
+        type="password"
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={submitAuth}>Submit</button>
+    </div>
+  );
 }
